674c7289cf1c5a759a1645aa7d66242319bf210c
jl917 <julong1988@naver.com>
2025-07-24 19:45:52 +0900

    fix: 플러그인 수정

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,11 +1,10 @@
 import * as path from "path";
 import { defineConfig, UserConfig } from "rspress/config";
 import { pluginFontOpenSans } from "rspress-plugin-font-open-sans";
-import dd from "./plugins/datadog";
-import clarity from "rspress-plugin-clarity";
 import pluginSitemap from "rspress-plugin-sitemap";
 import mermaid from "rspress-plugin-mermaid";
-import directives from 'rspress-plugin-directives';
+import directives from "rspress-plugin-directives";
+import { pluginLlms } from "@rspress/plugin-llms";
 
 const config: UserConfig = {
   root: path.join(__dirname, "docs"),
@@ -16,9 +15,7 @@ const config: UserConfig = {
   themeConfig: {
     enableContentAnimation: true,
     lastUpdated: true,
-    socialLinks: [
-      { icon: "github", mode: "link", content: "https://github.com/jl917" },
-    ],
+    socialLinks: [{ icon: "github", mode: "link", content: "https://github.com/jl917" }],
     enableScrollToTop: true,
   },
   plugins: [
@@ -26,19 +23,15 @@ const config: UserConfig = {
     pluginSitemap({ domain: "https://jl917.github.io" }),
     mermaid({ mermaidConfig: {} }),
     directives({
-      directive: 'giphy',
+      directive: "giphy",
       transformer: {
-        type: 'globalComponent',
-        getComponentName: (meta) => 'Giphy',
-        componentPath: path.join(__dirname, './components/Giphy.tsx'),
+        type: "globalComponent",
+        getComponentName: (meta) => "Giphy",
+        componentPath: path.join(__dirname, "./components/Giphy.tsx"),
       },
     }),
+    pluginLlms(),
   ],
 };
-
-if (process.env.NODE_ENV === "production") {
-  config.plugins.push(dd());
-  config.plugins.push(clarity("m8aieurh61"));
-}
 
 export default defineConfig(config);
